f76479a10c62386113e20480b51956723413aa3d
Jordi Perendreu <jordi.perendreu@gmail.com>
2024-07-14 19:51:35 +0200

    feat: Add more info in stripe errors

--- src/main/java/com/playtomic/tests/wallet/stripeclient/exception/StripeAmountTooSmallException.java
+++ src/main/java/com/playtomic/tests/wallet/stripeclient/exception/StripeAmountTooSmallException.java
@@ -1,4 +1,9 @@
 package com.playtomic.tests.wallet.stripeclient.exception;
 
 public class StripeAmountTooSmallException extends StripeServiceException {
+
+    public StripeAmountTooSmallException() {
+        super("Amount too small");
+    }
+
 }
